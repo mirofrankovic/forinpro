bdc6dbbe6528f6fd7478c36306761713797eed52
Miro Frankovic <miro.frankovic@gmail.com>
2022-03-21 21:45:07 +0000

    Modify code

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 class Miro:
     def __init__(self, name, age):
         self.name=name
-        self.age=age
+        self.my_year=age
         
     def my_Name(self):
         return self.name   
 
     def my_age(self):
-        return self.age       
+        return self.my_year
 
-f = Miro("hero", 30)   
+f = Miro("hero", 30)   #f is an object
 print(f.my_age())  
 z = Miro("zero", 40)
 print(z.my_Name())   
